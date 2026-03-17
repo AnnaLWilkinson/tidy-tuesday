241264f101a63fa7387bb2538f674ac492046101
Anna Wilkinson <48003521+AnnaLWilkinson@users.noreply.github.com>
2026-03-17 15:07:25 +1100

    add in processing and eda code

--- 2026/2026-03-17/tidy-tuesday-2026-03-17.R
+++ 2026/2026-03-17/tidy-tuesday-2026-03-17.R
@@ -37,3 +37,72 @@ monthly_mortality_data <- tuesdata$monthly_mortality_data
 #monthly_losses_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-03-17/monthly_losses_data.csv')
 #monthly_mortality_data <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-03-17/monthly_mortality_data.csv')
 
+
+
+# Summary -----------------------------------------------------------------
+
+summary(monthly_losses_data)
+summary(monthly_mortality_data)
+
+
+
+# Processing --------------------------------------------------------------
+range(monthly_losses_data$date)
+
+monthly_losses_data <-  monthly_losses_data %>% 
+  mutate(month   = lubridate::month(date),
+         year    = lubridate::year(date), 
+         monyear = ym(paste(year, "-", month)))
+
+tabyl(monthly_losses_data, month)
+tabyl(monthly_losses_data, year)
+
+# looks like geo_group duplicates data - so split
+
+sum(duplicated(c(monthly_losses_data$date, monthly_losses_data$losses)))
+
+list_of_dfs <- split(monthly_losses_data, monthly_losses_data$geo_group)
+
+monthly_losses_data_area   <-  list_of_dfs$area
+monthly_losses_data_county <-  list_of_dfs$county
+
+
+# Exploratory data analysis -----------------------------------------------
+
+monthly_losses_data_area %>% 
+  summarise(across(dead:other, sum), .by = monyear) %>% 
+  pivot_longer(-monyear,
+               names_to = "loss_cat",
+               values_to = "number") %>% 
+  
+  ggplot() + 
+  geom_bar(aes(x = monyear, 
+               y = number), 
+           stat = "identity") + 
+  facet_wrap(~ loss_cat, scales = "free_y")
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
