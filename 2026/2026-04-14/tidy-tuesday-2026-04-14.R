2e54fb56cf048effb4a3a9aeba3b7dd5cdafd4e7
Anna Wilkinson <48003521+AnnaLWilkinson@users.noreply.github.com>
2026-04-27 11:29:44 +1000

    Finish pretty plot and create png file

--- 2026/2026-04-14/tidy-tuesday-2026-04-14.R
+++ 2026/2026-04-14/tidy-tuesday-2026-04-14.R
@@ -70,18 +70,74 @@ birds_ships %>%
   summarise(sum_count = sum(count), .by = c(year, species_common_name)) %>% 
   arrange(year)
 
+birds_ships %>% 
+  summarise(n = n(), .by = time) %>% 
+  ggplot() + 
+  geom_bar(aes(x = time, 
+               y = n),
+           stat = "identity")
+
+birds_ships %>% 
+  summarise(n = n(), .by = time) %>% 
+  ggplot(aes(x = time, 
+               y = n)) + 
+  geom_point() + 
+  geom_line()
+
+
+birds_ships %>% 
+  summarise(n = n(), .by = c(observer,time)) %>% 
+  ggplot(aes(x = time, 
+             y = n,
+             group = observer,
+             colour = observer)) + 
+  geom_line()
+
+
+birds_ships %>% 
+  drop_na(season) %>% 
+  summarise(n = n(), .by = c(season,time)) %>% 
+  ggplot(aes(x = time, 
+             y = n,
+             group = season,
+             fill = season)) + 
+  geom_col(width = 500) +    # width must be in seconds - one hour wide is 3600 secs
+  scale_x_time() + 
+  facet_wrap(~ season)
 
 
 
+# Pretty plot -------------------------------------------------------------
 
 
+p <- birds_ships %>% 
+  drop_na(season) %>% 
+  summarise(n = n(), .by = c(season,time)) %>% 
+  ggplot(aes(x = time, 
+             y = n,
+             group = stringr::str_to_sentence(season),
+             fill = stringr::str_to_sentence(season))) + 
+  geom_col(width = 500,  # width must be in seconds - one hour wide is 3600 secs
+           fill = "darkgrey") +   
+  labs(y = "Count of bird observations (n)",
+       x = "Time of observation (24hr clock)",
+       fill = "") + 
+  scale_x_time(date_labels = "%H:%M") + 
+  theme_bw() +
+  theme(
+    
+    legend.position = "none",
+    strip.background = element_rect(fill = "navy"),
+    strip.text.x.top = element_text(face = "bold", 
+                                    colour = "white")
+  ) + 
+  facet_wrap(~ fct_relevel(stringr::str_to_sentence(season), "Summer","Autumn","Winter","Spring"))
 
+p
+# Export plot -------------------------------------------------------------
 
-
-
-
-
-
+ggsave(filename = "2026/2026-04-14/20260414.png",
+       plot = p)
 
 
 
