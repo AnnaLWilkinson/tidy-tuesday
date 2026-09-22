fc3ec3c4d80bdf9f361964b93383b0ceaba9367f
Anna Wilkinson <48003521+AnnaLWilkinson@users.noreply.github.com>
2026-09-22 13:21:32 +1000

    Process age and gender sheet

--- 2026/Worksafe Claims/worksafe_claims.R
+++ 2026/Worksafe Claims/worksafe_claims.R
@@ -50,11 +50,12 @@ claims_dfs <- excel_sheets(tmp) |>
   set_names() |>
   map(~ read_excel(tmp, sheet = .x))
 
-## Occupation
+
+#  Occupation  ------------------------------------------------------------
+
 claims_occupation_raw <- claims_dfs[["Occupation"]]
 
-
-# Process data  -----------------------------------------------------------
+## Process data  -----------------------------------------------------------
 
 new_names <- claims_occupation_raw |> 
   slice(4) |> 
@@ -74,6 +75,55 @@ claims_occupation_clean <- claims_occupation_raw |>
                values_to = "claims")
 
 
+
+
+# Age and gender ----------------------------------------------------------
+
+claims_age_gender_raw <- claims_dfs[["Age and gender"]]
+
+claims_age_gender_clean <- claims_age_gender_raw |> 
+  janitor::clean_names() |> 
+  mutate(gender = case_when(
+    
+          scheme_standardised_claims == "Female" ~ "female", 
+          scheme_standardised_claims == "Male"   ~ "male", 
+          scheme_standardised_claims == "I use a different term"   ~ "diff_term", 
+          scheme_standardised_claims == "Non-Binary/Gender Diverse" ~ "non_binary_diverse", 
+          scheme_standardised_claims == "Prefer not to say" ~ "prefer_not_say", 
+          scheme_standardised_claims == "Total" ~ "total", 
+          TRUE ~ NA_character_
+  )) |> 
+   fill(gender, .direction =  "down")
+
+new_names <- claims_age_gender_clean |> 
+  slice(5) |> 
+  janitor::clean_names() |> 
+  select(starts_with("x")) |> 
+  mutate(occupation = "age_group",
+         gender = "gender",
+         across(everything(), ~ str_replace_all(., "/", "_"))) |> 
+  select(occupation, starts_with("x"), gender) |> 
+  unlist()
+new_names
+
+claims_age_gender_clean <- claims_age_gender_clean |> 
+  setNames(new_names) |> 
+  slice(-(1:5))
+
+valid_age_group <- claims_age_gender_clean |> 
+  distinct(age_group) |> 
+  filter(str_detect(age_group, "-") | str_detect(age_group, "65+")| str_detect(age_group, "Under 15")| str_detect(age_group, "Not Stated"), 
+         !str_detect(age_group, "Non")) |> 
+  unlist()
+valid_age_group
+
+claims_age_gender_clean <-  claims_age_gender_clean |> 
+  filter(age_group %in% valid_age_group)
+
+claims_age_gender_clean <-  claims_age_gender_clean |> 
+  pivot_longer(-c(age_group, gender), 
+               names_to = "financial_year",
+               values_to = "claims")
 
 
 
